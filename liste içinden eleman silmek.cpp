a849bd322deb25cd8ed86e2fa162a0cc79468b15
Özgür Ay <42957556+OzgurJs1@users.noreply.github.com>
2023-10-12 18:55:40 +0300

    Update liste içinden eleman silmek.cpp

--- liste içinden eleman silmek.cpp
+++ liste içinden eleman silmek.cpp
@@ -1,42 +1,35 @@
-//liste içinden eleman silmek
+//liste iÃ§inden eleman silmek
 #include <stdio.h>
 #include <stdlib.h>
 
-// baðlý liste düðüm yapýsý
+
 struct node {
     int data;
     struct node *next;
 };
 
-// verilen elemaný listeden silen fonksiyon
 void delete_node(struct node **head_ref, int key) {
-    // baþlangýç düðümü
+    // baÃ¾langÃ½Ã§ dÃ¼Ã°Ã¼mÃ¼
     struct node* temp = *head_ref, *prev;
 
     // ilk eleman silinirse
     if (temp != NULL && temp->data == key) {
-        *head_ref = temp->next;   // baþlangýç düðümünü deðiþtir
-        free(temp);               // eski düðümü serbest býrak
+        *head_ref = temp->next;  
+        free(temp); 
         return;
     }
-
-    // silinecek elemaný bul
+
     while (temp != NULL && temp->data != key) {
         prev = temp;
         temp = temp->next;
-    }
-
-    // eleman bulunamadýysa
+    }
     if (temp == NULL) {
         return;
-    }
-
-    // elemaný listeden çýkar
+    }
     prev->next = temp->next;
-    free(temp);   // eski düðümü serbest býrak
+    free(temp);   // eski dÃ¼Ã°Ã¼mÃ¼ serbest bÃ½rak
 }
 
-// baðlý listeyi yazdýran fonksiyon
 void print_list(struct node *node) {
     while (node != NULL) {
         printf("%d ", node->data);
@@ -47,7 +40,6 @@ void print_list(struct node *node) {
 int main() {
     struct node *head = NULL;
 
-    // baðlý liste oluþtur
     head = (struct node*)malloc(sizeof(struct node));
     head->data = 1;
     head->next = (struct node*)malloc(sizeof(struct node));
@@ -58,8 +50,7 @@ int main() {
 
     printf("Onceki liste: ");
     print_list(head);
-
-    // elemaný sil
+
     delete_node(&head, 3);
 
     printf("\nSonraki liste: ");
